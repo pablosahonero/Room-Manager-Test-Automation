472220a7c4d4c6e7a85c1b5319689747325ad75a
pablosahonero <pablo.sahonero.2407@gmail.com>
2015-06-08 08:50:44 -0400

    Small changes

--- org/roommanager/test/CreateOutOfOrder.java
+++ org/roommanager/test/CreateOutOfOrder.java
@@ -3,13 +3,9 @@ package org.roommanager.test;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
-
 import java.util.concurrent.TimeUnit;
-
 import org.junit.*;
-
 import static org.junit.Assert.*;
-
 import org.openqa.selenium.*;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
